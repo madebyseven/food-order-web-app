33c22cbd41146d6d03fcd2f79793523fa4fd256b
colt <drownworb@gmail.com>
2024-09-13 15:57:21 +0800

    fix quantity update in cart context

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -35,15 +35,16 @@ function cartReducer(state, action) {
     );
     const existingCartItem = state.items[existingCartItemIndex];
 
+    const updatedItems = [...state.items];
+
     if (existingCartItem.quantity === 1) {
-      const updatedItems = [...state.items];
       updatedItems.splice(existingCartItemIndex, 1);
     } else {
-      const updateItem = {
+      const updatedItem = {
         ...existingCartItem,
         quantity: existingCartItem.quantity - 1,
       };
-      updateItems[existingCartItemIndex] = updateItem;
+      updatedItems[existingCartItemIndex] = updatedItem;
     }
     return { ...state, items: updatedItems };
   }
